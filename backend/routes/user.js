f200b1903e7421b972a98f4b7feccbd810f86a5f
abanwar1 <islamanwar031@gmail.com>
2024-12-12 00:04:52 +0530

    token generate and BookCard added

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/user")
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 //sing-up
 router.post("/sing-up", async (req,res) => {
   try {
@@ -30,9 +32,40 @@ router.post("/sing-up", async (req,res) => {
       .status(400)
       .json({message:"Password must me length of 6"});
     }
-    const newUser = new User ({username:username,email:email,password:password,address:address});
+    const hashPassword = await bcrypt.hash(password,10);
+    const newUser = new User ({username:username,email:email,password:hashPassword,address:address});
     await newUser.save();
     return res.status(200).json({message:"Signup Successfully"})
+  } catch (error) {
+    res.status(500).json({message:"Internal Server Error"});
+  }
+})
+//login
+router.post("/sing-in", async (req,res) => {
+  try {
+    const {username,password} = req.body;
+    const existingUser  = await User.findOne({username});
+    if(!existingUser){
+      res.status(400).json({message:"Invalid Credentials"})
+    }
+    await bcrypt.compare(password, existingUser.password,(err,data)=>{
+      if(data){
+        const authClaims = [
+          {
+            name:existingUser.username
+          },
+          {
+            role:existingUser.role
+          },
+        ]
+        const token = jwt.sign({authClaims},"bookifi123",{
+          expiresIn:"30d"
+        })
+        res.status(200).json({id:existingUser._id,role:existingUser.role,token:token});
+      } else {
+        res.status(400).json({message:"Invalid credentials"});
+      }
+    })
   } catch (error) {
     res.status(500).json({message:"Internal Server Error"});
   }
